97261a6ee98eb037629e6083882fe9763956cbaa
MightyBucket <rahuljhuree@hotmail.co.uk>
2026-02-23 19:20:18 +0000

    Added a weekly backup to google drive script for db_refresh container

--- db-refresh/entrypoint.sh
+++ db-refresh/entrypoint.sh
@@ -4,10 +4,15 @@ set -euo pipefail
 apt-get update -qq
 apt-get install -y -qq cron >/dev/null
 
-# Install cron job
-echo "${CRON} /scripts/refresh.sh >> /var/log/refresh.log 2>&1" > /etc/cron.d/db_refresh
+# Install cron job to refresh dev db from production db
+echo "${REFRESH_CRON} /scripts/refresh.sh >> /var/log/refresh.log 2>&1" > /etc/cron.d/db_refresh
 chmod 0644 /etc/cron.d/db_refresh
 crontab /etc/cron.d/db_refresh
+
+# Install cron job to backup db to drive
+echo "${BACKUP_CRON} /scripts/backup.sh >> /var/log/backup.log 2>&1" > /etc/cron.d/db_backup
+chmod 0644 /etc/cron.d/db_backup
+crontab /etc/cron.d/db_backup
 
 touch /var/log/refresh.log
 echo "db_refresh running with CRON='${CRON}'"
